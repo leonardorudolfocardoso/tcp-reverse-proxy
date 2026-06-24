799d9ca7985dd116399655bc61d9ef1e114d54ab
Leonardo C. <leonardorudolfocardoso@gmail.com>
2026-06-24 13:46:16 -0300

    feat: load balancer

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,66 @@
 use std::io;
-use std::sync::Arc;
+use std::sync::atomic::AtomicUsize;
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio_rustls::TlsConnector;
-use tokio_rustls::rustls::pki_types::InvalidDnsNameError;
-use tokio_rustls::rustls::{ClientConfig, RootCertStore, pki_types::ServerName};
 
 type BoxedError = Box<dyn std::error::Error + Send + Sync>;
 
-#[derive(Debug)]
-enum HandleError {
-    Io(io::Error),
-    Dns(InvalidDnsNameError),
-}
-
-impl From<io::Error> for HandleError {
-    fn from(value: io::Error) -> Self {
-        Self::Io(value)
-    }
-}
-impl From<InvalidDnsNameError> for HandleError {
-    fn from(value: InvalidDnsNameError) -> Self {
-        Self::Dns(value)
-    }
-}
-
-async fn handle(mut client: TcpStream, connector: TlsConnector) -> Result<u64, HandleError> {
-    let upstream = TcpStream::connect("example.com:443").await?;
-
-    let server_name = ServerName::try_from("example.com")?;
-    let mut upstream_tls = connector.connect(server_name, upstream).await?;
+async fn handle(mut client: TcpStream, server: &str) -> Result<u64, io::Error> {
+    let mut upstream = TcpStream::connect(server).await?;
 
     let mut buffer = vec![0u8; 8192];
     let n = client.read(&mut buffer).await?;
 
     let request = String::from_utf8_lossy(&buffer[..n]);
-    let request = request.replace("Host: 127.0.0.1:8080", "Host: example.com");
+    let request = request.replace("Host: 127.0.0.1:8080", format!("Host: {server}").as_str());
 
-    upstream_tls.write_all(request.as_bytes()).await?;
+    upstream.write_all(request.as_bytes()).await?;
 
-    let copied = tokio::io::copy(&mut upstream_tls, &mut client).await?;
+    let copied = tokio::io::copy(&mut upstream, &mut client).await?;
 
     Ok(copied)
+}
+
+struct LoadBalancer {
+    backends: Vec<String>,
+    counter: AtomicUsize,
+}
+
+impl LoadBalancer {
+    fn new(backends: Vec<String>) -> LoadBalancer {
+        LoadBalancer {
+            backends,
+            counter: AtomicUsize::new(0),
+        }
+    }
+    fn next(&self) -> String {
+        let index = self
+            .counter
+            .fetch_add(1, std::sync::atomic::Ordering::Relaxed)
+            % self.backends.len();
+        self.backends[index].clone()
+    }
+}
+
+impl<'a> FromIterator<&'a str> for LoadBalancer {
+    fn from_iter<T: IntoIterator<Item = &'a str>>(iter: T) -> Self {
+        Self::new(iter.into_iter().map(|i| i.to_owned()).collect())
+    }
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> std::result::Result<(), BoxedError> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    let mut roots = RootCertStore::empty();
-    roots.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
-
-    let config = ClientConfig::builder()
-        .with_root_certificates(roots)
-        .with_no_client_auth();
-
-    let connector = TlsConnector::from(Arc::new(config));
+    let lb = LoadBalancer::from_iter(["localhost:3001", "localhost:3002", "localhost:3003"]);
 
     loop {
         let (client, _) = listener.accept().await?;
-        let connector = connector.clone();
+        let server = lb.next();
 
         tokio::task::spawn(async move {
-            if let Err(err) = handle(client, connector).await {
+            if let Err(err) = handle(client, &server).await {
                 eprintln!("connection error: {err:?}");
             }
         });
